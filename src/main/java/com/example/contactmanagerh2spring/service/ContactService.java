7121b8b7d3a42eee8faa21777608b7a8fc1cfd10
Gaurav Barmola <gaurav101b@gmail.com>
2023-05-05 11:47:02 +0530

    Add h2 db and DAO

--- src/main/java/com/example/contactmanagerh2spring/service/ContactService.java
+++ src/main/java/com/example/contactmanagerh2spring/service/ContactService.java
@@ -1,6 +1,8 @@
 package com.example.contactmanagerh2spring.service;
 
 import com.example.contactmanagerh2spring.model.Contact;
+import com.example.contactmanagerh2spring.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
@@ -11,21 +13,22 @@ import java.util.List;
 @Component
 public class ContactService {
 
-    private static List<Contact> contactList = new ArrayList<>();
+//    private static List<Contact> contactList = new ArrayList<>();
+//
+//    static {
+//        contactList.add( new Contact("AAA Kumar", LocalDate.of(1980, 1, 1)));
+//        contactList.add( new Contact("BBB Kumar", LocalDate.of(1980, 1, 1)));
+//        contactList.add( new Contact("CCC Kumar", LocalDate.of(1980, 1, 1)));
+//    }
 
-    static {
-        contactList.add( new Contact("AAA Kumar", LocalDate.of(1980, 1, 1)));
-        contactList.add( new Contact("BBB Kumar", LocalDate.of(1980, 1, 1)));
-        contactList.add( new Contact("CCC Kumar", LocalDate.of(1980, 1, 1)));
-    }
-
+    @Autowired
+    private ContactRepository contactRepository;
 
     public List<Contact> getAllContacts() {
-        return contactList;
+        return (List<Contact>) contactRepository.findAll();
     }
 
     public Contact createContact(Contact contact) {
-        contactList.add(contact);
-        return contact;
+        return contactRepository.save(contact);
     }
 }
